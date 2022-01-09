f6570b7274b7efda2173d39c83cdbb43ecf70b49
Abdullah Tabish <70273959+abdullahtabish@users.noreply.github.com>
2022-01-10 03:32:22 +0530

    Add files via upload

---
+++ Dad Jokes/script.js
@@ -0,0 +1,20 @@
+const jokeElement = document.getElementById("joke");
+const jokeButton = document.getElementById("jokeBtn");
+
+jokeButton.addEventListener("click", generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com/", config);
+
+  const data = await res.json();
+
+  jokeElement.innerHTML = data.joke;
+}
